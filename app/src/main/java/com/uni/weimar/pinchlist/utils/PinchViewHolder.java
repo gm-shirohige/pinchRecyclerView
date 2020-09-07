29151fa8445e6933fdc1736a85180fbcb8e03a90
Mohammed <mu@diycam.com>
2020-09-07 22:02:26 +0530

    Update

--- app/src/main/java/com/uni/weimar/pinchlist/utils/PinchViewHolder.java
+++ app/src/main/java/com/uni/weimar/pinchlist/utils/PinchViewHolder.java
@@ -6,12 +6,14 @@ import android.widget.TextView;
 import com.uni.weimar.pinchlist.R;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class PinchViewHolder extends RecyclerView.ViewHolder implements isExpandable {
 
     int nestedListHeight = 0;
     TextView tv;
+    PinchList pv;
 
 
     /**
@@ -22,14 +24,39 @@ public class PinchViewHolder extends RecyclerView.ViewHolder implements isExpand
     public PinchViewHolder(@NonNull View itemView) {
         super(itemView);
         tv = itemView.findViewById(R.id.text_item);
+        pv = itemView.findViewById(R.id.sub_list);
     }
 
 
 
     public void expandList(int pixelGrowth){
+        String[] productCategories = {
+                "Sports",
+                "Clothing",
+                "Electronics",
+                "Groceries",
+                "Utilities",
+                "Home Appliances",
+                "Sports",
+                "Clothing",
+                "Electronics",
+                "Groceries",
+                "Utilities",
+                "Home Appliances",
+                "Sports",
+                "Clothing",
+                "Electronics",
+                "Groceries",
+                "Utilities",
+                "Home Appliances",
+        };
+        pv.setHasFixedSize(false);
+        pv.setLayoutManager(new LinearLayoutManager(itemView.getContext()));
+        pv.setPinchableAdapter(new PinchAdapter(productCategories));
+        pv.setVisibility(View.VISIBLE);
         tv.setText(tv.getText() + " Expanded");
-        tv.setHeight(500);
-        tv.setBackgroundColor(333);
+        pv.setMinimumHeight(300);
+        pv.setMinimumWidth(300);
     }
 
 }
